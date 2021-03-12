3f174e549301cb8df6aa931cd1544beb77fc7f87
Stephen Wan <stephen@stephenwan.com>
2021-03-11 16:45:43 -0800

    XXX: add sensors to split listener

--- app/src/split_listener.c
+++ app/src/split_listener.c
@@ -21,16 +21,31 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 int split_listener(const zmk_event_t *eh) {
     LOG_DBG("");
-    const struct zmk_position_state_changed *ev = as_zmk_position_state_changed(eh);
-    if (ev != NULL) {
-        if (ev->state) {
-            return zmk_split_bt_position_pressed(ev->position);
-        } else {
-            return zmk_split_bt_position_released(ev->position);
+    const struct zmk_position_state_changed *pos_ev;
+    if ((pos_ev = as_zmk_position_state_changed(eh)) != NULL) {
+        if (pos_ev != NULL) {
+            if (pos_ev->state) {
+                return zmk_split_bt_position_pressed(pos_ev->position);
+            } else {
+                return zmk_split_bt_position_released(pos_ev->position);
+            }
         }
     }
+
+#if ZMK_KEYMAP_HAS_SENSORS
+    const struct zmk_sensor_event *sensor_ev;
+    if ((sensor_ev = as_zmk_sensor_event(eh)) != NULL) {
+        if (sensor_ev  != NULL) {
+            // return zmk_split_bt_sensor_triggered(sensor_ev->sensor_number, sensor_ev->sensor);
+        }
+    }
+#endif /* ZMK_KEYMAP_HAS_SENSORS */
     return ZMK_EV_EVENT_BUBBLE;
 }
 
 ZMK_LISTENER(split_listener, split_listener);
 ZMK_SUBSCRIPTION(split_listener, zmk_position_state_changed);
+
+#if ZMK_KEYMAP_HAS_SENSORS
+ZMK_SUBSCRIPTION(split_listener, zmk_sensor_event);
+#endif /* ZMK_KEYMAP_HAS_SENSORS */
